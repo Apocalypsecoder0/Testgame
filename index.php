e938275fa4e104189a4b99ae48f617c8d4973978
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-12 17:23:03 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include 'db.php';
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$user_id = $_SESSION['user_id'];
+$stmt = $pdo->prepare("SELECT * FROM game_data WHERE user_id = ?");
+$stmt->execute([$user_id]);
+$game_data = $stmt->fetch();
+?>
+
+<h1>Welcome to the Game!</h1>
+<p>Command Center Level: <?= $game_data['command_center_level'] ?></p>
+<a href="logout.php">Logout</a>
+<a href="command_center.php">Command Center</a>
+<a href="attack.php">Attack</a>
