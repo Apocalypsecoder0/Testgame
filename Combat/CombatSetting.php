114dc856870e0b021e9ebda28d34a95adca5056a
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-13 17:28:45 -0500

    Update CombatSetting.php

--- Combat/CombatSetting.php
+++ Combat/CombatSetting.php
@@ -15,6 +15,30 @@ class CombatSetting {
     public function calculateBattleOutcome() {
         // Logic for calculating battle outcome
         // Return result
+        
+public function calculateBattleOutcome() {
+    $fleetStrength = $this->calculateStrength($this->fleet);
+    $defenseStrength = $this->calculateStrength($this->defense);
+    $enemyStrength = $this->calculateStrength($this->enemyFleet);
+
+    $totalStrength = $fleetStrength + $defenseStrength;
+
+    if ($totalStrength > $enemyStrength) {
+        return "Victory";
+    } elseif ($totalStrength < $enemyStrength) {
+        return "Defeat";
+    } else {
+        return "Draw";
+    }
+}
+
+private function calculateStrength($units) {
+    $strength = 0;
+    foreach ($units as $unit) {
+        $strength += $unit['power']; // Assuming each unit has a 'power' attribute
+    }
+    return $strength;
+
     }
 }
 ?>
