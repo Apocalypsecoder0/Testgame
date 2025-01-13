43eafb415a6d36b68434b0ad2af0f4eb662c367f
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-13 17:25:40 -0500

    Create Crafting.php

---
+++ Craft/Crafting.php
@@ -0,0 +1,16 @@
+<?php
+// Crafting.php
+
+class Crafting {
+    private $resources;
+
+    public function __construct($resources) {
+        $this->resources = $resources;
+    }
+
+    public function craftItem($itemId) {
+        // Check resources and craft item
+        // Return success or failure
+    }
+}
+?>
