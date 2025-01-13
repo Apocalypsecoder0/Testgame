a188bcf1c7d85de66c2acd449c1ba153a3c3fc4a
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-13 17:04:54 -0500

    Update general.php

--- menu/general.php
+++ menu/general.php
@@ -2,4 +2,15 @@
 // general.php
 echo "<h1>General Settings</h1>";
 // Add general settings content here
+
+// general.php
+echo "<h1>General Settings</h1>";
+echo "<form method='post' action='save_general.php'>";
+echo "<label for='site_name'>Site Name:</label>";
+echo "<input type='text' id='site_name' name='site_name' required><br>";
+echo "<label for='site_description'>Site Description:</label>";
+echo "<textarea id='site_description' name='site_description' required></textarea><br>";
+echo "<input type='submit' value='Save Settings'>";
+echo "</form>";
+
 ?>
