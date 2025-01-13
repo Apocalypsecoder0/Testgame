d7f68b9776b2828d00a568761d1f59d345ec60c5
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-13 15:00:54 -0500

    Create about.php

---
+++ header/about.php
@@ -0,0 +1,24 @@
+<?php
+$title = "About Us";
+include('header.php');
+?>
+
+<h1>About Us</h1>
+<p>Our mission is to...</p>
+<p>Our vision is to...</p>
+<h2>History</h2>
+<p>Founded in...</p>
+<h2>Meet Our Team</h2>
+<ul>
+    <li>Team Member 1</li>
+    <li>Team Member 2</li>
+</ul>
+
+<div class="testimonials">
+    <h3>What Our Users Say</h3>
+    <blockquote>"Great service!" - User</blockquote>
+</div>
+
+<a href="https://socialmedia.com">Follow us on social media</a>
+
+<?php include('footer.php'); ?>
