177d457e0e432d086d76fd69d5705c59c52cc1a4
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-12 23:34:06 -0500

    Create  impressum.php

---
+++ impressum.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<title>Impressum</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="favicon.ico" />
+<style type="text/css">
+<!--
+html, body, a {
+    padding:0px;
+    margin:0px;
+    background-color:#000000;
+    color:#b1daf2;
+    font-family: verdana, arial, helvetica, sans-serif;
+    font-size:12px;
+}
+
+a {
+    text-align:left;
+}
+
+a:hover {
+    text-decoration:underline;
+    color:#ffffff;
+}
+
+
+h1 {
+    font-size:18px;
+    color:#061229;
+    padding:0px;
+    padding-left:8px;
+    margin:0px;
+}
+
+h2 {
+    font-size:13px;
+    margin:0px;
+    margin-bottom:10px;
+    color:#061229;
+}
+
+h3 {
+    font-size:12px;
+    font-weight:bold;
+    margin:0px;
+    margin-bottom:10px;
+
+}
+
+hr {
+    margin:13px 0px;
+    padding:0px;
+    color:#061229;
+    background-color:#061229;
+    border:0px;
+    border-bottom:1px dashed #9EBDE4;
+    clear:both;
+    width:100%;
+}
+
+* html hr {
+    margin:5px 0px;
+}
+-->
+</style>
+</head>
+<body>
+<table width="520" align="center" cellspacing="0" cellpadding="0">
+    <tr>
+        <td>
+        <br/><br/>	<br /><br />
+<b><u>Reasons for Project Creation</u></b><br /><br />
+
+List of reasons why the Redesign is unsuitable for normal gameplay: 
+<ul>
+<li>Pages are heavier. Excessive traffic load.</li>
+<li>Page rendering is very slow due to an abundance of images, styles, and overloaded JavaScript.</li>
+<li>It is impossible to play properly from mobile devices.</li>
+<li>Abundance of unfixable bugs. Constant emergence of new bugs.</li>
+<li>Game balance changes (new planet activities in Overview (which are also buggy), Astrophysics, planet relocation, etc.).</li>
+</ul>
+
+As a result, the following is lost: 
+<ul>
+<li>The ability to adequately manage fleets. Now raising a fleet in 3-4 seconds or simultaneously slowing down 3-4 SCs is practically impossible.</li>
+<li>With the corrected activity, a whole layer of the game is lost - the so-called "Ninja," where a player refreshes the Overview and suddenly catches an attacker through Gates.</li>
+<li>Simple resource gathering from all planets or saving data on moons takes 2-3 times longer than before.</li>
+<li>The chance to move a fleet in emergencies from a mobile device is reduced to zero.</li>
+</ul>
+
+<br /><br />
+<b><u>Differences from the Original</u></b><br /><br />
+
+What will not be in this project: 
+<ul>
+<li> The game will correspond to version 0.1.5</li>
+<li>There will be no paid Dark Matter, but functions for Dark Matter found in expeditions will be available (Officers and Merchant).</li>
+</ul>
+
+Errors in the original version 0.1.5 will be fixed: 
+<ul>
+<li>Buggy planet when attempting to colonize the 10th planet. Instead, there will be an Abandoned Colony.</li>
+<li>Target planet activity after returning your fleet to the starting planet.</li>
+<li>Fleet cloning (raising fleet after an attack).</li>
+<li>Negative resources (ordering construction during an attack).</li>
+<li>Slowing down returning fleets recalled from SC.</li>
+<li>Accelerating return from expeditions.</li>
+<li>And others...</li>
+</ul>
+
+<br /><br />
+<b><u>Development</u></b><br /><br />
+
+Google Code: <a href="http://opensource</a>
+
+<br /><br />
+Admin Dev Team: Apocalypsecoder0
+
+        </table>
+        <br/>
+        <!-- lang: en | content: impress --></body>
+</html>
