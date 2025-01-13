41e5b5f3527ed7c3205fbf73438a93d8c7075e85
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-13 17:34:15 -0500

    Update WeeklyMission.php

--- weekly/WeeklyMission.php
+++ weekly/WeeklyMission.php
@@ -15,6 +15,29 @@ class WeeklyMission {
     public function completeMission($missionId) {
         // Logic to complete a mission
         // Update user rewards
+
+public function completeMission($missionId) {
+    // Check if the mission exists
+    if (isset($this->missions[$missionId])) {
+        // Mark the mission as completed
+        $this->missions[$missionId]['completed'] = true;
+
+        // Update user rewards
+        $this->updateUserRewards($this->missions[$missionId]['reward']);
+        
+        return "Mission completed and rewards updated.";
+    } else {
+        return "Mission not found.";
+    }
+}
+
+private function updateUserRewards($reward) {
+    // Logic to update user rewards in the database
+    // Example: $this->user->addRewards($reward);
+    $this->user->addRewards($reward);
+    
+}
+        
     }
 }
 ?>
