9db6772c868f3745b59345553233bbb6fc14d35f
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-13 15:02:04 -0500

    Create community.php

---
+++ header/community.php
@@ -0,0 +1,17 @@
+<?php
+$title = "Community";
+include('header.php');
+?>
+
+<h1>Community Engagement</h1>
+<p>Join our forums and discussion boards!</p>
+
+<h2>Community Guidelines</h2>
+<p>Please adhere to our guidelines...</p>
+
+<div class="events">
+    <h3>Upcoming Events</h3>
+    <p>Event details...</p>
+</div>
+
+<?php include('footer.php'); ?>
